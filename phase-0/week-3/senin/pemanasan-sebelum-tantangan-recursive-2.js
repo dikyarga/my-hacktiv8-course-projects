150c5f364824cb8408b4874e85529657ff985296
Diky Arga Anggara <dikyarga.id@gmail.com>
2016-12-12 22:48:41 +0700

    add correct task with correct solution

--- phase-0/week-3/senin/pemanasan-sebelum-tantangan-recursive-2.js
+++ phase-0/week-3/senin/pemanasan-sebelum-tantangan-recursive-2.js
@@ -1,34 +1,25 @@
-// Looping sederhana mengalikan dengan angka sebelumnya, hingga angkat terkecil 1
+
+// Rekrusif sederhana mengalikan dengan angka sebelumnya, hingga angkat terkecil 1
 // Sample
 // Input totalSum(5)
 // Output 5 * 4 * 3 * 2 * 1 = 120
-
 function totalSum (number) {
-  // Buat varibel sementara / temporary
-  var total;
-  // Lakukan perulang sebanyak jumlah angka
-  for (var i = number; i >= 1; i--) {
-    // Kalikan dan simpan hasilnya ke variabel total
-    total *= number;
+  // Cek apakah ini angka terakhir / terkecil yang ingin ikut dihitung
+  if (number == 1) {
+    // Jika kembalikan nilainya
+    return 1;
+  } else {
+    // Jika tidak, maka terus lakukan rekrusif dengan memanggil fungsinya sendiri
+    return number * totalSum(number - 1);
   }
-  // Kembali kan hasilnya dengan melempar variabel total
-  return total;
 }
 
 console.log(totalSum(5));
 
-// Looping dengan mengalikan dengan angka sebelumnya dengan selisih paramater kedua
+// Rekrusif dengan mengalikan dengan angka sebelumnya dengan selisih paramater kedua
 // Sample
 // Input : totalLompat(12, 5)
 // Output : 12 * 7 * 2 = 168
-
-// function totalLompat  (number, dif) {
-//   var total;
-//   for (var i = 0; i < number; i++) {
-//     total *= number;
-//   }
-//   return total;
-// }
 function totalLompat (number, dif) {
   // Cek apakah ini angka terakhir / terkecil yang ingin ikut dihitung
   if (number == 1) {
