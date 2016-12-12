80502527d049fab741a25d3abc26c44f46c7d2e3
Diky Arga Anggara <dikyarga.id@gmail.com>
2016-12-12 23:15:37 +0700

    Fixing the Monday task:

--- phase-0/week-3/senin/pemanasan-sebelum-tantangan-recursive-looping.js
+++ phase-0/week-3/senin/pemanasan-sebelum-tantangan-recursive-looping.js
@@ -5,11 +5,12 @@
 
 function totalSum (number) {
   // Buat varibel sementara / temporary
-  var total;
+  // Assign dengan nilai 1, supaya tida menjadi NaN
+  var total = 1;
   // Lakukan perulang sebanyak jumlah angka
   for (var i = number; i >= 1; i--) {
     // Kalikan dan simpan hasilnya ke variabel total
-    total *= number;
+    total *= i;
   }
   // Kembali kan hasilnya dengan melempar variabel total
   return total;
@@ -22,25 +23,16 @@ console.log(totalSum(5));
 // Input : totalLompat(12, 5)
 // Output : 12 * 7 * 2 = 168
 
-// function totalLompat  (number, dif) {
-//   var total;
-//   for (var i = 0; i < number; i++) {
-//     total *= number;
-//   }
-//   return total;
-// }
-function totalLompat (number, dif) {
-  // Cek apakah ini angka terakhir / terkecil yang ingin ikut dihitung
-  if (number == 1) {
-    // Jika kembalikan nilainya
-    return 1;
-  // Cek kembali, apakah 'number' lebih besar dari dif (pembeda / selisih ke angka selanjutnya)
-  } else if (number > dif) {
-    // Jika iya, maka terus lakukan rekrusif dengan memanggil fungsinya sendiri
-    return number * totalLompat(number - dif, dif);
-  } else {
-    // Jika tidak, maka balikan angka tersebut, agar menjadi nilai kemabalian pada proses rekrusif terakhir
-    return number;
+function totalLompat  (number, dif) {
+  // Buat varibel sementara / temporary
+  // Assign dengan nilai 1, supaya tida menjadi NaN
+  var total = 1;
+  // Lakukan perulang sebanyak jumlah angka dengan selisih sebanyak dif
+  for (var i = number; i >= 1; i -= dif) {
+    // Kalikan dan simpan hasilnya ke variabel total
+    total *= i;
   }
+  // Kembali kan hasilnya dengan melempar variabel total
+  return total;
 }
 console.log(totalLompat(12, 5));
