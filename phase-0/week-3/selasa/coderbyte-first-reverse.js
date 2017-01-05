d49305770e4f4e51909e392924e1d8d0fc064c6f
Diky Arga <dikyarga.id@gmail.com>
2017-01-05 21:52:16 +0700

    add coderbyte - first reverse

---
+++ phase-0/week-3/selasa/coderbyte-first-reverse.js
@@ -0,0 +1,13 @@
+// function reverseString(str) {
+//     return str.split("").reverse().join("");
+// }
+// console.log(reverseString("hello"));
+function FirstReverse(str) {
+
+  // code goes here
+  return str.split("").reverse().join("");
+
+}
+console.log(FirstReverse("hello"));
+// keep this function call here
+// FirstReverse(readline());
