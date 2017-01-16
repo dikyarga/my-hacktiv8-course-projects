e270954be29b3e27734ef0a39b9848b4cea78dfa
Diky Arga <dikyarga.id@gmail.com>
2017-01-17 01:42:55 +0700

    add other product coderbyte challange

---
+++ phase-0/week-3/rabu/coderbyte-other-products.js
@@ -0,0 +1,19 @@
+function OtherProducts(arr) {
+  var str = '';
+  for (var i = 0; i < arr.length; i++) {
+    var temp = 1;
+    for (var j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        temp *= arr[j];
+      }
+    }
+    str += temp;
+    if (i !== arr.length - 1) {
+      str += '-';
+    }
+  }
+  // code goes here
+  return str;
+
+}
+console.log(OtherProducts([1,2,3,100]));
