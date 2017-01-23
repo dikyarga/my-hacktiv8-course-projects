d8d9d814517a6148a29c92ad598e4b8a98fd4a8d
Diky Arga <dikyarga.id@gmail.com>
2017-01-23 23:03:23 +0700

    mengakses isi HTML dari DOM

--- phase-0/week-5/senin/js-simple-dom-script.js
+++ phase-0/week-5/senin/js-simple-dom-script.js
@@ -7,3 +7,11 @@ console.log(pageBoxElements);
 var pageHeadings = document.getElementsByTagName("h1");
 // Menyeleksi DOM berdasarkan tag <h1> dan menampungnya ke dalam variabel. Isinya merupakan array dari object HTML element
 console.log(pageHeadings);
+
+var pageTitleElementsContent = pageTitleElement.innerHTML;
+console.log('isi <div id="page-title"> :' + pageTitleElementsContent);
+// isi <div id="page-title"> adalah Sample Page Title
+
+var pageBoxElementsContent = pageBoxElements.innerHTML;
+console.log('isi <div class="page-box"> :' + pageBoxElementsContent);
+// isi <div class="page-box"> adalah undefined!
