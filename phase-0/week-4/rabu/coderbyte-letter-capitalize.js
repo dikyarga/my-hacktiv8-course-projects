991028b4c04f43380d26c566c45d88e6ce56ac2d
Diky Arga <dikyarga.id@gmail.com>
2017-01-23 15:22:19 +0700

    add notes at the code

--- phase-0/week-4/rabu/coderbyte-letter-capitalize.js
+++ phase-0/week-4/rabu/coderbyte-letter-capitalize.js
@@ -1,4 +1,5 @@
 function LetterCapitalize(str) {
+// note \w cari kata dalam kalimat
   return str.replace(/\w\S*/g,
     function(txt){
       return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
