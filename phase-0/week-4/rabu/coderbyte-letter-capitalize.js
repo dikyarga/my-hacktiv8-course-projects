7033006506503c05bf27e622a00150f7500c02a0
Diky Arga <dikyarga.id@gmail.com>
2017-01-23 15:14:37 +0700

    add coderbyte letter capitalize test

---
+++ phase-0/week-4/rabu/coderbyte-letter-capitalize.js
@@ -0,0 +1,9 @@
+function LetterCapitalize(str) {
+  return str.replace(/\w\S*/g,
+    function(txt){
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+console.log(LetterCapitalize('hello world'));
