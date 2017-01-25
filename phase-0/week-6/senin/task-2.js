e5981c0d6ba22b7130a992b8d8b46e484b2282aa
Diky Arga <dikyarga.id@gmail.com>
2017-01-25 17:17:21 +0700

    Task of W6 D1 part 2

---
+++ phase-0/week-6/senin/task-2.js
@@ -0,0 +1,15 @@
+const multiply = (num1, num2) => {
+  return num1 * num2;
+}
+
+const divide = (num1, num2) => {
+  return num1 / num2;
+}
+
+const doubleMe = num => {
+  return num * 2;
+}
+
+console.log(multiply(5,2));
+console.log(divide(10,2));
+console.log(doubleMe(4));
